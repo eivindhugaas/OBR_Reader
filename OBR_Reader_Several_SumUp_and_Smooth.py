ab1be9aa3297f976a53123e5a02e224ae94fd19e
Eivind Hugaas <eivinhug@ntnu.no>
2018-09-04 09:20:43 +0200

    Fiber noise stuff

---
+++ OBR_Reader_Several_SumUp_and_Smooth.py
@@ -0,0 +1,80 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from itertools import product, combinations
+import csv
+import sys
+from ReaderFunctions.ReaderFunctions import readers as rdr
+from ReaderFunctions.ReaderFunctions import smooth as smt
+import os
+
+startdistance=0.0 #mm
+length=120.0 #mm
+key='E01_fibre 3_'
+prefix=key
+path=r'C:\Users\eivinhug\OneDrive - NTNU\PhD_Backup\NTNU\PhD\Testing\Laminate_E\OBR_Files\E01_FatigueTest_2_28082018'
+rdr=rdr()
+smt=smt()
+
+highest_numbers=np.arange(13,174,10)
+start_heres=np.arange(5,166,10)
+zeroreading=4
+
+load_file_number = str(zeroreading).zfill(4)
+file=prefix+load_file_number+'_Lower.txt'
+doods=rdr.Lower_Reader(location='%s\%s' %(path, file))
+print(doods)
+PastStrain=doods[1]
+#Result=rdr.SeriesReader(location=r'C:\Users\eivinhug\NTNU\PhD\Testing\Laminate_D\OBR_Files\D01_Temsion_07062018',key=key,startdistance=startdistance,length=length)
+
+for i in range(0,len(highest_numbers)):
+    
+    StrainSummed=doods[1]
+    start=start_heres[i]
+    stop=highest_numbers[i]
+
+    filename="Output_Summed_Smooth_"+str(prefix)+str(start)+"_"+str(stop)+".txt"
+    
+    print(path+filename)
+    
+    thefile = open(os.path.join(path,filename), 'w')
+    thefile.write("Length (m) \t") 
+    for i in range(start,stop+1,1):
+        thefile.write("Measurement %s (microstrain)\t"%int(i))  
+    thefile.write("\n")
+    AllStrain=[]
+    Strain=[]
+    for i in range(start,stop+1,1):
+        load_file_number = str(i).zfill(4)
+        file=prefix+load_file_number+'_Lower.txt'
+        dood=rdr.Lower_Reader(location='%s\%s' %(path, file))
+        if i==start:
+            Length=dood[0]
+            AllStrain.append(Length)
+        Strain=dood[1]
+        StrainSummed=[StrainSummed[a]+Strain[a] for a in range(len(Strain))]#[x + y for x, y in zip(Strain,PastStrain)]
+        StrainSummedSmooth=smt.SmootherAvg(Length,StrainSummed)
+        AllStrain.append(StrainSummedSmooth)
+
+    print(AllStrain)
+    for t in range(len(Length)-1): #-1 to avoid number of datpoints issues.
+        for d in range(len(AllStrain)):
+            if AllStrain[d]==[]:
+                thefile.write("0.000000")
+                thefile.write("\t")
+            else:
+                thefile.write("%.6f"%AllStrain[d][t])
+                thefile.write("\t")
+        thefile.write("\n")    
+    
+    
+    #ResultSmooth=rdr.Smoother(Result=Result[0],FiberNumbers=Result[1],Fibersforsmoothing=[9],algorithm='threshold')
+    #rdr.Plotter(Result=Result[0],FiberNumbers=Result[1])
+    #rdr.Plotter(Result=ResultSmooth,FiberNumbers=Result[1])
+    #rdr.ContourPlotter(Result=ResultSmooth,FiberNumbers=Result[1],FiberPositions=Result[2])
+    #rdr.SurfacePlotter(Result=Result[0],FiberNumbers=Result[1],FiberPositions=Result[2])
+    #rdr.SurfacePlotter(Result=ResultSmooth,FiberNumbers=Result[1],FiberPositions=Result[2])
+    #rdr.Writer(Result=Result[0],FiberNumbers=Result[1],Filename=key)
+    #rdr.ContourPlotter(Result=ResultSmooth,FiberNumbers=Result[1],FiberPositions=Result[2])
+    
--- ReaderFunctions/ReaderFunctions.py
+++ ReaderFunctions/ReaderFunctions.py
@@ -328,3 +328,97 @@ class smooth:
                     avgFilter = np.asarray(avgFilter),
                     stdFilter = np.asarray(stdFilter))
 
+    def SmootherAvg(self,Length,Strain):
+    
+        Diff=[]
+    
+        for i in range(len(Strain)): #finds the difference between measurements, the first difference is between the first and the first. The second is first to second amd so on.
+            m=Strain[i]
+            if i==0:
+                lastm=Strain[0]   
+            else:
+                lastm=Strain[i-1]   
+            Diffi=m-lastm
+            Diff.append(Diffi)   
+        LDiff=[]
+    
+        for i in range(len(Length)): #finds the difference between measurements, the first difference is between the first and the first. The second is first to second amd so on.
+            m=Length[i]
+            if i==0:
+                lastm=Length[0]   
+            else:
+                lastm=Length[i-1]   
+            Diffi=m-lastm
+            LDiff.append(Diffi)
+    
+        Ln=sum(LDiff)/len(LDiff)
+    
+        smoothn=[]
+    
+        for i in range(len(Diff)): #sorts what measurements shuld be average assumed (0.0) and kept as is (1.0).
+            if i==0 or i==len(Diff)-1:
+                n=1.
+            elif abs(Diff[i])>187.5*Ln*1000:
+                n=0.
+            elif abs(Strain[i])<50.:
+                n=1.
+            else:
+                n=1.
+            smoothn.append(n)
+    
+        smoothslist=[]
+    
+        for i in range(len(smoothn)): #adds another 1.0 in ranges longer than two 0.0's
+            currentsmoothn=smoothn[i]
+            lastsmoothn=1.0
+            nextsmoothn=1.0  
+            if i>0 and i<len(smoothn)-1:
+                currentsmoothn=smoothn[i]
+                lastsmoothn=smoothn[i-1]
+                nextsmoothn=smoothn[i+1]
+            if currentsmoothn==0.0 and lastsmoothn==0.0:
+                if nextsmoothn==1.0:
+                    smooths=1.0
+            else:
+                smooths=currentsmoothn
+            smoothslist.append(smooths)
+    
+        n=0
+        avgloc=[]
+        avg=[]
+        for i in range(len(smoothslist)):
+            if smoothslist[i]==0.0 and i!=0 and i!=len(smoothslist)-1:
+                n=n+1
+                if n==1:
+                    firstavg=Strain[i-1]
+                    avg.append(firstavg)
+                    avgloc.append(i)
+                if smoothslist[i+1]==1.0:
+                    secondavg=Strain[i+1]
+                    n=0
+                    avg.append(secondavg)
+                    avgloc.append(i)
+    
+        t=0
+        a=0
+        SmoothStrains=[]
+        for i in range(len(Strain)):
+            Smoothstrain=Strain[i]
+            if i>=avgloc[t] and i<=avgloc[t+1]:
+                a=a+1
+                Smoothstraininc=((avg[t+1]-avg[t])/(avgloc[t+1]-avgloc[t]+2))
+                Smoothstrain=(avg[t]+(Smoothstraininc*a))
+            if i>=avgloc[t+1] and t<(len(avgloc)-2):
+                t=t+2
+                a=0
+    
+            SmoothStrains.append(Smoothstrain)
+    
+        SuperSmoothStrains=[]
+        for i in range(len(SmoothStrains)):
+            if abs(SmoothStrains[i]-Strain[i])<800.:
+                SuperSmoothStrains.append(Strain[i])
+            else:
+                SuperSmoothStrains.append(SmoothStrains[i])    
+                
+        return SuperSmoothStrains
